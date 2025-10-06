c600a4d1a45e5defe18b9feeef95148c358146dc
danii <danii@DANIIL>
2025-10-06 23:19:47 +0500

    Add project files.

---
+++ Tyuiu.MedvedevDP.Sprint3.Task0.V19.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.MedvedevDP.Sprint3.Task0.V19.Lib;
+namespace Tyuiu.MedvedevDP.Sprint3.Task0.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService dataService = new DataService();
+
+            int startValue = 1;
+            int stopValue = 10;
+
+            double res = dataService.GetSumSeries(startValue, stopValue);
+            double wait = 4.399;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MedvedevDP.Sprint3.Task0.V19/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.MedvedevDP.Sprint3.Task0.V19.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService dataService = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int startValue = 1;
+        int stopValue = 10;
+
+        Console.WriteLine($"Старт шага = {startValue}");
+        Console.WriteLine($"Конец шага = {stopValue}");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double res = dataService.GetSumSeries(startValue, stopValue);
+
+        Console.WriteLine($"Сумма ряда = {res}");
+        Console.ReadKey();
+    }
+}
