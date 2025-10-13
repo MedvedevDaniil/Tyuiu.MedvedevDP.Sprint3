26babfc6e29eb2f5d04bfaaa69e91b70eb071078
danii <danii@DANIIL>
2025-10-13 18:58:45 +0500

    task1

---
+++ Tyuiu.MedvedevDP.Sprint3.Task1.V7.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MedvedevDP.Sprint3.Task1.V7.Lib;
+
+namespace Tyuiu.MedvedevDP.Sprint3.Task1.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 9;
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = -0.93;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MedvedevDP.Sprint3.Task1.V7/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.MedvedevDP.Sprint3.Task1.V7.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        double value = 0.25;
+        int startValue = 1;
+        int stopValue = 9;
+        Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+        Console.ReadKey();
+    }
+}
