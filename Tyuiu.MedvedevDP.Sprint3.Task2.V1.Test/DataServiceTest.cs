593b092386859b54b56f91ef93c6fe88668802ec
danii <danii@DANIIL>
2025-10-13 19:31:08 +0500

    task2

---
+++ Tyuiu.MedvedevDP.Sprint3.Task2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.MedvedevDP.Sprint3.Task2.V1.Lib;
+
+namespace Tyuiu.MedvedevDP.Sprint3.Task2.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 10;
+            double res = ds.GetSumSeries(startValue, stopValue);
+            double wait = 0.353;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MedvedevDP.Sprint3.Task2.V1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.MedvedevDP.Sprint3.Task2.V1.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int startValue = 1;
+        int stopValue = 10;
+        Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(startValue, stopValue));
+        Console.ReadKey();
+    }
+}
